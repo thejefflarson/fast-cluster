14640c36eb837d7887a46d533accbbd307e1452f
Jeff Larson <thejefflarson@gmail.com>
2013-02-05 14:03:05 -0500

    simple main

--- src/fast_cluster.cc
+++ src/fast_cluster.cc
@@ -6,11 +6,6 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  if(argc != 2) {
-    cerr << "usage: fast_cluster <file>" << std::endl;
-    exit(1);
-  }
-
   ifstream ifs = ifstream(argv[1]);
 
   string buf;
@@ -20,7 +15,7 @@ int main(int argc, char *argv[]){
     ifs.seekg(0, ios::beg);
     ifs.read(&buf[0], buf.size());
     ifs.close();
-    cout << fast_cluster::SimHash(buf, 5) << "\t" << argv[1] << endl;
+    cout << fast_cluster::SimHash(buf, argv[2]) << "\t" << argv[1] << endl;
   } else {
     cout << "couldn't find " << argv[1] << endl;
   }
